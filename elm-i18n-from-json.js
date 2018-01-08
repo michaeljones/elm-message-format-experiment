8bbca89e26b97b3284c83d7c17afc9b7a8be2a0d
Michael Jones <m.pricejones@gmail.com>
2018-01-08 19:29:52 +0000

    Format with prettier

--- elm-i18n-from-json.js
+++ elm-i18n-from-json.js
@@ -1,19 +1,16 @@
+const fs = require('fs')
 
-const fs = require('fs');
-
-const flatten = require('flat');
-const parse = require('messageformat-parser').parse;
-const program = require('commander');
-const _ = require('lodash');
-
+const flatten = require('flat')
+const parse = require('messageformat-parser').parse
+const program = require('commander')
+const _ = require('lodash')
 
 function capitalise(word) {
-    return word[0].toUpperCase() + word.slice(1);
+    return word[0].toUpperCase() + word.slice(1)
 }
 
-
 function escape(key) {
-    return key === 'type' ? 'type_' : key;
+    return key === 'type' ? 'type_' : key
 }
 
 function extractValue(entry) {
@@ -25,86 +22,93 @@ function extractValue(entry) {
 
 function dotToCaps(name) {
     const fragments = name.split('.')
-    return fragments[0] + fragments.slice(1).map(capitalise).join('');
+    return (
+        fragments[0] +
+        fragments
+            .slice(1)
+            .map(capitalise)
+            .join('')
+    )
 }
 
 function generate(filepath, options) {
+    const fileContents = fs.readFileSync(filepath)
 
-    const fileContents = fs.readFileSync(filepath);
-
-    let fileJson = JSON.parse(fileContents);
+    let fileJson = JSON.parse(fileContents)
 
     if (options.context) {
         fileJson = _.cloneDeepWith(fileJson, extractValue)
     }
 
-    const flatJson = flatten(fileJson);
+    const flatJson = flatten(fileJson)
 
     function toToken(key) {
-        return "Tr" + key.split('.').map(capitalise).join('');
+        return (
+            'Tr' +
+            key
+                .split('.')
+                .map(capitalise)
+                .join('')
+        )
     }
 
     function toArgs(value) {
         const parsed = parse(value).filter(entry => typeof entry !== 'string')
-        return _.uniq(parsed.map(entry => entry.arg));
+        return _.uniq(parsed.map(entry => entry.arg))
     }
 
-    const tokens = _.toPairs(flatJson)
-        .map(([key, value]) => {
-            const rawArgs = toArgs(value);
-            let args = '';
-            if (rawArgs.length) {
-                const defs = rawArgs.map(name => `${name} : String`).join(', ');
-                args = ` { ${defs} }`;
-            }
-            return toToken(key) + args;
-        });
+    const tokens = _.toPairs(flatJson).map(([key, value]) => {
+        const rawArgs = toArgs(value)
+        let args = ''
+        if (rawArgs.length) {
+            const defs = rawArgs.map(name => `${name} : String`).join(', ')
+            args = ` { ${defs} }`
+        }
+        return toToken(key) + args
+    })
 
+    const entries = _.toPairs(flatJson).map(([key, value]) => {
+        return dotToCaps(escape(key))
+    })
 
-    const entries = _.toPairs(flatJson)
-        .map(([key, value]) => {
-            return dotToCaps(escape(key));
-        });
+    const defaults = _.toPairs(flatJson).map(([key, value]) => {
+        return `${dotToCaps(escape(key))} = "${key}"`
+    })
 
-    const defaults = _.toPairs(flatJson)
-        .map(([key, value]) => {
+    const lookups = _.toPairs(flatJson).map(([key, value]) => {
+        return `${dotToCaps(escape(key))} = ${toToken(key)}`
+    })
 
-            return `${dotToCaps(escape(key))} = "${key}"`;
-        });
+    const decodes = _.toPairs(flatJson).map(([key, value]) => {
+        const hasDots = key.includes('.')
+        if (hasDots) {
+            const path =
+                '[' +
+                key
+                    .split('.')
+                    .map(entry => `"${entry}"`)
+                    .join(', ') +
+                ']'
+            return `        |> optionalAt ${path} string "${key}"`
+        } else {
+            return `        |> optional "${key}" string "${key}"`
+        }
+    })
 
-    const lookups = _.toPairs(flatJson)
-        .map(([key, value]) => {
+    const strings = _.toPairs(flatJson).map(([key, value]) => {
+        const token = toToken(key)
+        const rawArgs = toArgs(value)
+        const method = rawArgs.length ? 'formatWithArgs' : 'format'
+        const string = `strings.${dotToCaps(escape(key))}`
+        let args = ''
+        if (rawArgs.length) {
+            args = ' args'
+        }
 
-            return `${dotToCaps(escape(key))} = ${toToken(key)}`;
-        });
+        return `        ${token}${args} ->\n            MessageFormat.${method} strings.code__ ${string}${args}`
+    })
 
-    const decodes = _.toPairs(flatJson)
-        .map(([key, value]) => {
-            const hasDots = key.includes('.');
-            if (hasDots) {
-                const path = '[' + key.split('.').map(entry => `"${entry}"`).join(', ') + ']'
-                return `        |> optionalAt ${path} string "${key}"`;
-            }
-            else {
-                return `        |> optional "${key}" string "${key}"`;
-            }
-        });
-
-    const strings = _.toPairs(flatJson)
-        .map(([key, value]) => {
-            const token = toToken(key);
-            const rawArgs = toArgs(value);
-            const method = rawArgs.length ? 'formatWithArgs' : 'format';
-            const string = `strings.${dotToCaps(escape(key))}`;
-            let args = '';
-            if (rawArgs.length) {
-                args = ' args';
-            }
-
-            return `        ${token}${args} ->\n            MessageFormat.${method} strings.code__ ${string}${args}`;
-        });
-
-    const moduleName = options.module || 'Translation';
+    const moduleName = options.module || 'Translation'
 
     const output = `module ${moduleName} exposing (..)
 
@@ -157,101 +161,91 @@ defaultLanguage =
 trText : Language -> Translation -> Html msg
 trText strings slug =
     text (translate strings slug)
-`;
+`
 
-    return output;
+    return output
 }
-
 
 function generateAction(config, outputPath, options) {
-    const output = generate(config, options);
-    console.log(`Writing to ${outputPath}`);
-    fs.writeFileSync(outputPath, output);
+    const output = generate(config, options)
+    console.log(`Writing to ${outputPath}`)
+    fs.writeFileSync(outputPath, output)
 }
-
 
 function diffAction(configPath, resultPath, options) {
-    const output = generate(configPath, options);
-    const resultContent = fs.readFileSync(resultPath, 'utf8');
+    const output = generate(configPath, options)
+    const resultContent = fs.readFileSync(resultPath, 'utf8')
 
     if (output !== resultContent) {
-        console.error('Differences detected');
-        process.exit(1);
+        console.error('Differences detected')
+        process.exit(1)
     }
 
-    process.exit(0);
+    process.exit(0)
 }
 
-
 function validateAction(configPath) {
+    const configContents = fs.readFileSync(configPath)
 
-    const configContents = fs.readFileSync(configPath);
+    const configJson = JSON.parse(configContents)
 
-    const configJson = JSON.parse(configContents);
+    const pairs = _.toPairs(configJson.languages).map(([language, filename]) => {
+        const contents = fs.readFileSync(filename)
 
-    const pairs = _.toPairs(configJson.languages)
-        .map(([language, filename]) => {
+        const json = JSON.parse(contents)
 
-            const contents = fs.readFileSync(filename);
+        const flatJson = flatten(json)
 
-            const json = JSON.parse(contents);
+        const keys = _.keys(flatJson)
 
-            const flatJson = flatten(json);
+        return [filename, keys]
+    })
 
-            const keys = _.keys(flatJson);
-
-            return [filename, keys];
-        });
-
-    const firstFilename = pairs[0][0];
-    const firstKeys = pairs[0][1];
+    const firstFilename = pairs[0][0]
+    const firstKeys = pairs[0][1]
 
     const results = _(pairs)
         .drop(1)
         .map(([filename, keys]) => {
-            const diffA = keys.filter(key => !_.includes(firstKeys, key));
-            const diffB = firstKeys.filter(key => !_.includes(keys, key));
+            const diffA = keys.filter(key => !_.includes(firstKeys, key))
+            const diffB = firstKeys.filter(key => !_.includes(keys, key))
 
             if (diffA.length) {
-                console.log(`The following keys are present in ${filename} and not ${firstFilename}`);
-                console.log(diffA.join(', '));
+                console.log(`The following keys are present in ${filename} and not ${firstFilename}`)
+                console.log(diffA.join(', '))
             }
 
             if (diffB.length) {
-                console.log(`The following keys are present in ${firstFilename} and not ${filename}`);
-                console.log(diffB.join(', '));
+                console.log(`The following keys are present in ${firstFilename} and not ${filename}`)
+                console.log(diffB.join(', '))
             }
 
-            return diffA.length > 0 || diffB.length > 0;
+            return diffA.length > 0 || diffB.length > 0
         })
         .filter(value => value)
-        .value();
+        .value()
 
     if (results.length) {
-        process.exit(1);
+        process.exit(1)
     }
 }
 
-
 function main(args) {
+    program
+        .command('generate <file> <output>')
+        .option('-s, --settings <name>', 'Which config to use')
+        .option('-m, --module <name>', 'Name of generated module')
+        .option('-c, --context', "Uses the 'context' format for strings")
+        .action(generateAction)
 
     program
-        .command("generate <file> <output>")
-        .option("-s, --settings <name>", "Which config to use")
-        .option("-m, --module <name>", "Name of generated module")
-        .option("-c, --context", "Uses the 'context' format for strings")
-        .action(generateAction);
+        .command('diff <config> <output>')
+        .option('-s, --settings <name>', 'Which config to use')
+        .action(diffAction)
 
-    program
-        .command("diff <config> <output>")
-        .option("-s, --settings <name>", "Which config to use")
-        .action(diffAction);
+    program.command('validate <config>').action(validateAction)
 
-    program
-        .command("validate <config>")
-        .action(validateAction);
-
-    program.parse(args);
+    program.parse(args)
 }
 
-main(process.argv);
+main(process.argv)
